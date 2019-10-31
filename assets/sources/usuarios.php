0979b279fe3782e51a22f5e063f432246ee94cf0
kkadeu <tadeu.rangel97@gmail.com>
2019-10-31 17:27:23 -0300

    Funções Usuário

--- assets/sources/usuarios.php
+++ assets/sources/usuarios.php
@@ -9,7 +9,37 @@ class Usuarios{
 
     function __construct()
     {
+        $nome = NULL;
+        $email = NULL;
+        $cpf = NULL;
+        $senha = NULL;
 
+    }
+
+    function insereUsuario()
+    {
+        $usuario = new Usuario;();
+
+
+        $usuario.nome = $_POST["nome"];
+        $usuario.email = $_POST["email"];
+        $usuario.cpf = $_POST["cpf"];
+        $usuario.senha = $_POST["senha"];
+
+        $this->insereUsuarioBD($cliente);
+    }
+
+    function insereUsuarioBD(Usuario $usuario)
+    {
+        $con = mysql_connect('localhost','root','');
+        mysql_select_db('marcenaria_bd', $con)
+
+        $query = "INSERT INTO usuarios (nome, senha, cpf, endereco) VALUES ('$usuarios.nome', 
+        '$usuarios.senha', '$usuarios.cpf','$usuarios.endereco') ";
+        $result = mysql_query($query);
+        
+        if ($result)
+        
     }
 
 }
