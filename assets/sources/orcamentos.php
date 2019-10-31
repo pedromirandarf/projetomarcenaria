d50b159125a9af87a45b508f9ba587627dd26e4a
kkadeu <tadeu.rangel97@gmail.com>
2019-10-31 18:03:16 -0300

    Métodos Orçamento

--- assets/sources/orcamentos.php
+++ assets/sources/orcamentos.php
@@ -11,7 +11,23 @@ class Orcamento{
 
     function __construct()
     {
-        
+        $data = NULL;
+        $validade = NULL;
+        $precoTotal = NULL;
+        $obs = NULL;
+        $idUsuario = NULL;
+        $idCliente = NULL;
+    }
+
+    function insereOrcamentoBD(Orcamento $orcamento)
+    {
+        $con = mysql_connect('localhost','root','');
+        mysql_select_db('marcenaria_bd', $con)
+
+        $query = "INSERT INTO orcamento (data, validade, preco_total, obs, clientes_id, usuario_id) 
+        VALUES ('$orcamento.data', '$orcamento.validade', '$orcamento.precoTotal',
+        '$orcamento.obs','$orcamento.idCliente', '$orcamento.idUsuario') ";
+        $result = mysql_query($query);
     }
 }
 
